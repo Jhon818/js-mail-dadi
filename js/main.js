a86bc56618f4905341ba37a0d242ceecf1a637f4
giov <pintogiovanni818@gmail.com>
2021-07-16 15:59:59 +0200

    esercizio 1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// **Mail**
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+// Chiedi all’utente la sua email
+prompt(Email);
+// controlla che sia nella lista di chi può accedere
+var Email = ["peppo@gmail.com", "qualchemail@libero.it", "Emailemail.live.com"];
+// stampa un messaggio appropriato sull’esito del controllo.
+for (var i = 0; Email < Email.length; i++) {
+    
+    (Email[i]);
+}
+
