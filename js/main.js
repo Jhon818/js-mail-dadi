5b6257a66080697606a82f614d3781001851cd8b
giov <pintogiovanni818@gmail.com>
2021-07-18 16:41:05 +0200

    mail

--- js/main.js
+++ js/main.js
@@ -5,13 +5,15 @@
 
 
 // Chiedi all’utente la sua email
-prompt(Email);
+var Email = prompt(Email);
 // controlla che sia nella lista di chi può accedere
 var Email = ["peppo@gmail.com", "qualchemail@libero.it", "Emailemail.live.com"];
 // stampa un messaggio appropriato sull’esito del controllo.
 for (var i = 0; i < Email.length; i++) {
+
+    if (Email = Email[0]) {
+        alert("Accesso Autorizzato");
+    }
     
-    
-    alert(Email[i]);
 }
 
